5d3ab7d1b51dcff29635e61d13a104d4d2e939d8
Maxim Chernyak <max@bitsonnet.com>
2012-06-29 02:00:43 -0400

    Add a stub metadata file to enable install via librarian-chef.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Scott M. Likens"
+maintainer_email "scott@likens.us"
+license          "Apache 2.0"
+version          "1.0.0"
+
+description      "Resources for manipulating sysctl."
